585ff9698ffa41534894a8cc8f727da68de743ba
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-11-30 16:12:22 +0700

    Update PetManager.php

--- src/NatureBlessing/pet/PetManager.php
+++ src/NatureBlessing/pet/PetManager.php
@@ -16,6 +16,5 @@ class PetManager {
 
     public function summonSpirit(Player $player): void {
         $player->sendMessage(TF::GREEN . "A nature spirit has been summoned to protect you!");
-        // Logic for spawning the pet (e.g., using an EntityFactory or custom entity)
     }
 }
