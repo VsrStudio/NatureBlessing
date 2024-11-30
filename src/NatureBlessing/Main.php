8cbd80a213de787244c49dc157b11e9026710ac9
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-11-30 16:22:44 +0700

    Update Main.php

--- src/NatureBlessing/Main.php
+++ src/NatureBlessing/Main.php
@@ -3,41 +3,56 @@
 namespace NatureBlessing;
 
 use pocketmine\plugin\PluginBase;
-use NatureBlessing\weather\WeatherManager;
-use NatureBlessing\gardening\GardenManager;
-use NatureBlessing\pet\PetManager;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 
 class Main extends PluginBase {
 
-    private WeatherManager $weatherManager;
-    private GardenManager $gardenManager;
-    private PetManager $petManager;
-
     protected function onEnable(): void {
-        $this->saveResource("config.yml");
-        $this->saveResource("lang/en.yml");
-        $this->saveResource("lang/id.yml");
-
-        $this->weatherManager = new WeatherManager($this);
-        $this->gardenManager = new GardenManager($this);
-        $this->petManager = new PetManager($this);
-
-        $this->getLogger()->info("NatureBlessing has been enabled!");
-    }
-
-    protected function onDisable(): void {
-        $this->getLogger()->info("NatureBlessing has been disabled!");
+        $this->getServer()->getCommandMap()->register("natureblessing", new NatureBlessingCommand($this));
     }
 
     public function getWeatherManager(): WeatherManager {
-        return $this->weatherManager;
+        return new WeatherManager($this);
     }
 
     public function getGardenManager(): GardenManager {
-        return $this->gardenManager;
+        return new GardenManager($this);
     }
 
     public function getPetManager(): PetManager {
-        return $this->petManager;
+        return new PetManager($this);
+    }
+}
+
+class NatureBlessingCommand extends Command {
+
+    private Main $plugin;
+
+    public function __construct(Main $plugin) {
+        parent::__construct("nbweather", "Manage weather effects", "/nbweather <rain|storm|sunny>", []);
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("This command can only be used by players.");
+            return;
+        }
+
+        if (empty($args)) {
+            $sender->sendMessage("Please specify a weather type (rain, storm, sunny).");
+            return;
+        }
+
+        $weather = strtolower($args[0]);
+
+        if ($weather === "rain" || $weather === "storm" || $weather === "sunny") {
+            $this->plugin->getWeatherManager()->applyWeatherEffects();
+            $sender->sendMessage("Weather has been changed to $weather.");
+        } else {
+            $sender->sendMessage("Invalid weather type.");
+        }
     }
 }
